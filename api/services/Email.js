4249390615dabe29aa272f986b703e11a0fdc098
Douglas DeVicariis <kootg04@gmail.com>
2016-08-26 12:18:13 -0700

    Email Progress

--- api/services/Email.js
+++ api/services/Email.js
@@ -15,8 +15,14 @@ module.exports = {
             attachment: 
             [{
                 data:'<html>' +
-                '<body>' +
-                '<img src="http://solarmada.com/images/logo/Grey.png" height="100" width="100" />' +  
+                '<body style="font-align: center;">' +
+                '<img src="http://solarmada.com/images/logo/Grey.png" height="400" width="400"/>' +  
+                '<h1>Thank you for registering with <br>Sol Armada!</h1>' +  
+                '<p>After confirming your registration, we will evaluate your account and apply a badge that confirms that you are a member of Sol Armada. Allow up to 48 hours for confirmation. If you don’t see anything after 48 hours, please contact an administrator on our Discord server.</p>' + 
+                '<p>Please confirm your registration by clicking the button below.</p>' +  
+                '<p><a href="http://' + sails.config.local.url + '/confirm?code="' + user.confirmationCode + '>Confirm Your Email</a></p> ' + 
+                '<p>If the link above does not work, please copy and paste the following line in your browser URL address bar.</p>' +
+                '<p>http://' + sails.config.local.url + '/confirm?code=' + user.confirmationCode + '</p>' +
                 '</body>' + 
                 '</html>', 
             alternative:true}]
