6ac0e08da9d5a0d42f41dec164aaef178104a80e
Koo <kootg04@gmail.com>
2016-08-28 11:08:58 -0700

    Minor Fixes

--- api/services/Email.js
+++ api/services/Email.js
@@ -7,6 +7,32 @@ var server = email.server.connect({
 module.exports = {
     register: function(user, cb) {
         server.send({
+            text:    "Please confirm your registration by using the link below.\r\nhttp://" + (sails.config.url || "localhost") + "/confirm?code=" + user.confirmationCode, 
+            from:    "Sol Armada <donotreply@solarmada.com>", 
+            to:      (user.username || "New Sol Armada Member") + " <" + user.email + ">",
+            subject: "Verify Email",
+            attachment: 
+            [{
+                data:'<html>' +
+                    '<body style="font-align: center;">' +
+                    '<img src="http://solarmada.com/images/logo/Grey.png" height="400" width="400"/>' +
+                    '<h1>Thank you for registering with Sol Armada!</h1>' +
+                    '<p>After confirming your registration, we will evaluate your account and apply a badge that confirms that you are a member of Sol Armada. Allow up to 48 hours for confirmation. If you don’t see anything after 48 hours, please contact an administrator on our Discord server.</p>' + 
+                    '<p>Please confirm your registration by clicking the button below.</p>' +
+                    '<p><a href="http://' + (sails.config.url || 'localhost') + '/confirm?code="' + user.confirmationCode + '">Confirm Your Email</a></p> ' +
+                    '<p>If the link above does not work, please copy and paste the following line in your browser URL address bar.</p>' +
+                    '<p>http://' + (sails.config.url || 'localhost') + '/confirm?code=' + user.confirmationCode + '</p>' +
+                    '</body>' + 
+                    '</html>', 
+                alternative:true
+            }]
+        }, function(err, message) {
+            return cb(err);
+        });
+    },
+
+    confirmed: function(user, cb) {
+        server.send({
             text:    "", 
             from:    "Sol Armada <donotreply@solarmada.com>", 
             to:      (user.username || "New Sol Armada Member") + " <" + user.email + ">",
@@ -14,17 +40,14 @@ module.exports = {
             attachment: 
             [{
                 data:'<html>' +
-                '<body style="font-align: center;">' +
-                '<img src="http://solarmada.com/images/logo/Grey.png" height="400" width="400"/>' +  
-                '<h1>Thank you for registering with Sol Armada!</h1>' +  
-                '<p>After confirming your registration, we will evaluate your account and apply a badge that confirms that you are a member of Sol Armada. Allow up to 48 hours for confirmation. If you don’t see anything after 48 hours, please contact an administrator on our Discord server.</p>' + 
-                '<p>Please confirm your registration by clicking the button below.</p>' +  
-                '<p><a href="http://' + (sails.config.url || 'localhost') + '/confirm?code="' + user.confirmationCode + '">Confirm Your Email</a></p> ' + 
-                '<p>If the link above does not work, please copy and paste the following line in your browser URL address bar.</p>' +
-                '<p>http://' + (sails.config.url || 'localhost') + '/confirm?code=' + user.confirmationCode + '</p>' +
-                '</body>' + 
-                '</html>', 
-            alternative:true}]
+                    '<body style="font-align: center;">' +
+                    '<img src="http://solarmada.com/images/logo/Grey.png" height="400" width="400"/>' +
+                    '<h1>Thank you for confirming your email!</h1>' +
+
+                    '</body>' +
+                    '</html>',
+                alternative:true
+            }]
         }, function(err, message) {
             return cb(err);
         });
